3157f052cbf0c908d37d6ad9c995af88caf7a04b
aardnsyhs <ardiansyahdian755@gmail.com>
2025-01-16 11:46:26 +0700

    feat: addLazyLoading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        Model::preventLazyLoading();
+        Paginator::useTailwind();
     }
 }
