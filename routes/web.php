ddd430dec3c302b09b3e55777ab173aba63b2090
aardnsyhs <ardiansyahdian755@gmail.com>
2025-01-22 10:36:08 +0700

    feat: add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,25 @@ Route::get('/', function () {
     return view('auth.login');
 })->middleware('guest');
 
-Route::resource('produk', ProdukController::class)->middleware('auth');
-Route::resource('user', UserController::class)->middleware('auth');
-Route::resource('stok', StokController::class)->middleware('auth');
-Route::resource('pembelian', PembelianController::class)->middleware('auth');
-Route::get('search', [ProdukController::class, 'search'])->name('produk.search');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('produk', ProdukController::class);
+    Route::resource('stok', StokController::class);
+    Route::resource('pembelian', PembelianController::class);
+
+    Route::get('search', [ProdukController::class, 'search'])->name('produk.search');
+
+    Route::prefix('profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::middleware('admin')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+        Route::resource('user', UserController::class);
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
