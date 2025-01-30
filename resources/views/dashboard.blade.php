51c8db740c2c097bb039196c4d0e72b884b766da
aardnsyhs <ardiansyahdian755@gmail.com>
2025-01-30 11:53:12 +0700

    refactor: change table ui for transaction history

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -63,7 +63,7 @@
             <div class="bg-white dark:bg-gray-800 shadow-lg rounded-2xl p-6">
                 <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-4">Riwayat Transaksi</h2>
                 <div class="overflow-x-auto bg-white dark:bg-gray-900 shadow-lg rounded-lg p-4">
-                    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    {{-- <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                         <thead>
                             <tr class="bg-gray-100 dark:bg-gray-800">
                                 <th
@@ -105,6 +105,47 @@
                                         Tidak ada transaksi dalam periode ini.
                                     </td>
                                 </tr>
+                            @endforelse
+                        </tbody>
+                    </table> --}}
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Nama Pelanggan
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Tanggal Transaksi
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Total Harga
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Aksi
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($riwayatTransaksi as $transaksi)
+                                <tr
+                                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $transaksi->pelanggan->nama_pelanggan }}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ $transaksi->tanggal_penjualan }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Rp.{{ number_format($transaksi->total_harga, 2) }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#"
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail
+                                            Transaksi</a>
+                                    </td>
+                                </tr>
+                            @empty
                             @endforelse
                         </tbody>
                     </table>
