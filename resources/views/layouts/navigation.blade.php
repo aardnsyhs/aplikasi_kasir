cfcad07f97cb85317f515dec846e3ed70368a1d2
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-02-01 13:27:27 +0700

    feat: route home based on user role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
+                    @if(Auth::user()->role === 'admin')
                     <a href="{{ route('dashboard') }}">
                         <img src="{{ asset('assets/logo.png') }}" alt="logo"
                             class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
+                    @else
+                    <a href="{{ route('pembelian.index') }}">
+                        <img src="{{ asset('assets/logo.png') }}" alt="logo"
+                            class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                    @endif
                 </div>
 
                 <!-- Navigation Links -->
